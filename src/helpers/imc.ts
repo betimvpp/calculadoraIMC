961232fffaf979edb25f51a84b4f4caf659ad52d
betimvpp <105385037+betimvpp@users.noreply.github.com>
2022-07-26 14:52:21 -0300

    Update imc.ts

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -16,7 +16,7 @@ export const levels:Level[]=[
 export const calculateImc = (height: number, weight:number)=>{
     const imc = weight/(height*height);
     for(let i in levels){
-        if(imc>=levels[i].imc[0] && imc< levels[i].imc[i]){
+        if(imc>=levels[i].imc[0] && imc< levels[i].imc[1]){
             let levelCopy:Level = {...levels[i]};
 
             levelCopy.yourImc= parseFloat(imc.toFixed(2));
